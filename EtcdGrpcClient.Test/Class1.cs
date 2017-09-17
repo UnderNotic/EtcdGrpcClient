cc793ebde7eee799bf0640ff0d3eb55ba7e7f301
bambusek7 <bambusek7@gmail.com>
2017-09-17 15:25:05 +0200

    init commit

---
+++ EtcdGrpcClient.Test/Class1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace EtcdGrpcClient.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+       
+    }
+}
